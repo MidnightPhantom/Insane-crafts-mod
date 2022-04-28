d7617683608f786a397651a28f403b2d891b0f16
MidnightPhantom <96172170+MidnightPhantom@users.noreply.github.com>
2022-04-28 15:32:59 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- Crafts here
 
-if minetest.get_modpath(
+if minetest.get_modpath("default") then
 minetest.register_craft({
               output = ("default:stone_with_mese"),
               recipie = {{"default:stone", "default:stone", "default:stone"},
