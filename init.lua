2d02717054bb3c28e29ec0d851090b3fc527e2ce
MidnightPhantom <96172170+MidnightPhantom@users.noreply.github.com>
2022-04-28 15:31:27 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
 -- Crafts here
 
-if minetest.get
+if minetest.get_modpath(
 minetest.register_craft({
-              output = ("default:stone_with_diamond
+              output = ("default:stone_with_mese"),
+              recipie = {{"default:stone", "default:stone", "default:stone"},
+                         {"default:stone", "default:mese_crystal", "default:stone"},
+                         {"default:stone", "default:stone", "default:stone"},
+                       }
+      })
+
