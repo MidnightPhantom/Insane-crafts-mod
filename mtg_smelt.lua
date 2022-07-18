63992dd7ce4c477f5f223aa768c57f4ecab4f34f
MidnightPhantom <96172170+MidnightPhantom@users.noreply.github.com>
2022-07-18 16:39:37 +0530

    Create mtg_smelt.lua

---
+++ mtg_smelt.lua
@@ -0,0 +1,7 @@
+minetest.register_craft({
+                  type = "cooking",
+                  output = "default:river_water_source",
+                  recipie = "default:water_source",
+                      })
+
+                 
