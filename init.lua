b5fca67da98608c7a004d03eb08825d9d00a3792
MidnightPhantom <96172170+MidnightPhantom@users.noreply.github.com>
2022-07-18 16:58:58 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,3 +7,4 @@ insane_crafts = {};
 local modpath = minetest.get_modpath(minetest.get_current_modname());
 dofile(modpath.."/mtg_crafts.lua"); --has each and every craft for mtg
 dofile(modpath.."/mcl_crafts.lua"); --every mcl crafting recipe will be here
+dofile(modpath.."/mtg_smelt.lua"); -- has some smelting recipies
