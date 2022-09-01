ad4d40c134148afd9a5e9337104eda61e5a99207
MidnightPhantom <96172170+MidnightPhantom@users.noreply.github.com>
2022-09-01 12:27:41 +0530

    Update mtg_smelt.lua

--- mtg_smelt.lua
+++ mtg_smelt.lua
@@ -2,6 +2,11 @@ minetest.register_craft({
                   type = "cooking",
                   output = "default:river_water_source",
                   recipe = "default:ice",
-                      })
+               })
 
-                 
+minetest.register_craft({
+                  type = "cooking",
+                  output = "bucket:bucket_river_water",
+                  recipe = "bucket:bucket_water",
+                })
+
